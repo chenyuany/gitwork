279ef242ac427fac05290919455a04eab55ac6d0
chenyuany <1584216202@qq.com>
2018-02-26 13:21:28 +0800

    整合版

--- testSuite/test_036_routing_suite.py
+++ testSuite/test_036_routing_suite.py
@@ -3,8 +3,8 @@ u'''
 #文件名：
 #被测软件版本号：V2.8.1
 #作成人：李择优
-#生成日期：2018/1/31
-#模块描述：网卡配置
+#生成日期：2018/2/1
+#模块描述：路由配置
 #历史修改记录
 #修改人：
 #修改日期：
@@ -24,38 +24,37 @@ sys.path.append("/testIsomp/testData/")
 from _testDataPath import dataFileName
 
 sys.path.append("/testIsomp/testCase/network/")
-from test_network import NetworkCard
+from test_routing import RoutingConfiguration
 
 sys.path.append("/testIsomp/testSuite")
 from common_suite_file import CommonSuiteData,setDriver
 
 import unittest
 
-class NetworkCardSuite(unittest.TestCase):
+class RoutingSuite(unittest.TestCase):
     def setUp(self):
         #定义驱动
         self.browser = setDriver().set_driver()
         self.commonSuite = CommonSuiteData(self.browser)
-        self.net = NetworkCard(self.browser)
+        self.rout = RoutingConfiguration(self.browser)
         self.initDriver = initDriver()
         
         #前置条件
-        self.commonSuite.network_card_module_prefix_condition()
+        self.commonSuite.routing_module_prefix_condition()
         
     def test_network_card(self):
-        #设置网卡
-        self.net.set_network_card_001()
-        #清空网卡
-        self.net.del_network_card_002()
-        #校验网卡
-        self.net.check_network_card_003()
+        #添加路由
+        self.rout.add_routing_001()
+        #校验路由
+        self.rout.check_routing_002()
+        #删除路由
+        self.rout.del_routing_003()
        
     def tearDown(self):
         #后置条件
-        self.commonSuite.network_card_module_post_condition()
+        self.commonSuite.routing_module_post_condition()
         #关闭浏览器
         self.initDriver.close_driver(self.browser)
         
 if __name__ == "__main__":
     unittest.main()
-
